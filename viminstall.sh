0343f4babbca213ce18dae2cbedbf0fad559daa7
Jopa Maciej-HJPQ48 <maciej.jopa@motorolasolutions.com>
2018-06-27 15:41:32 +0200

    correction in instalation script

---
+++ viminstall.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd ..
+mv myvim/ ~/.vim
+cd ~/.vim
+
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+echo "submodules init & cloning"
+echo `git submodule init`
+echo `git submodule update`
+ln -s vimrc ~/.vimrc
+
